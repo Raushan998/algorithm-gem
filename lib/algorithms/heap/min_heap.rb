986d4d3a196daeb696db4ace1b4fde8e3d90780a
Raushan Kumar Raman <your.email@example.com>
2024-09-01 17:00:40 +0530

    refactored old code for algorithms gem

--- lib/algorithms/heap/min_heap.rb
+++ lib/algorithms/heap/min_heap.rb
@@ -1,69 +1,12 @@
 # frozen_string_literal: true
 
+require_relative "./helper"
 module Algorithms
   module Heap
     class MinHeap
-      attr_reader :arr
-
+      include Helper
       def initialize
-        @arr = []
-      end
-
-      def push(element)
-        @arr.push(element)
-        shift_up
-      end
-
-      def pop
-        return if @arr.empty?
-
-        last_idx = @arr.length - 1
-        @arr[last_idx], @arr[0] = @arr[0], @arr[last_idx]
-        @arr.pop
-        shift_down
-      end
-
-      def top
-        @arr[0]
-      end
-
-      def size
-        @arr.length
-      end
-      
-      private
-      def shift_up
-        _idx = @arr.length - 1
-        while _idx.positive?
-          parent_idx = (_idx - 1) / 2
-          break unless @arr[parent_idx] > @arr[_idx]
-
-          @arr[parent_idx], @arr[_idx] = @arr[_idx], @arr[parent_idx]
-
-          _idx = parent_idx
-        end
-      end
-
-      def shift_down
-        current_index = 0
-
-        while current_index < @arr.length
-          left_child_index = 2 * current_index + 1
-          right_child_index = 2 * current_index + 2
-          smallest_index = current_index
-
-          if left_child_index < @arr.length && @arr[left_child_index] < @arr[smallest_index]
-            smallest_index = left_child_index
-          end
-          if right_child_index < @arr.length && @arr[right_child_index] < @arr[smallest_index]
-            smallest_index = right_child_index
-          end
-
-          break if smallest_index == current_index
-
-          @arr[current_index], @arr[smallest_index] = @arr[smallest_index], @arr[current_index]
-          current_index = smallest_index
-        end
+        super(:>)
       end
     end
   end
